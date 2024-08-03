4a4dd0165be5db5b194d2138a59082157d0634a5
Allan Cleysson <allancleysson@gmail.com>
2024-08-03 12:04:47 -0300

    habilitando video e screenshot cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: true,
+  screenshotOnRunFailure: true,
   e2e: {
     baseUrl: 'https://serverest.dev',
     setupNodeEvents(on, config) {
